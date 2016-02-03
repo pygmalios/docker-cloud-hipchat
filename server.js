9983e06b6765f01c136741a3c2f18684c5d84093
Jan Antala <jan@antala.sk>
2016-02-03 17:50:22 +0100

    Send multiple message types

--- server.js
+++ server.js
@@ -12,6 +12,41 @@ var WebSocket = require('faye-websocket');
 var HipChatClient = require('hipchat-client');
 var hipchat = new HipChatClient(HIPCHAT_API_KEY);
 
+var sendContainerMessage = function(msg) {
+
+    sendMessage(JSON.stringify(msg));
+};
+
+var sendServiceMessage = function(msg) {
+
+    sendMessage(JSON.stringify(msg));
+};
+
+var sendStackMessage = function(msg) {
+
+    sendMessage(JSON.stringify(msg));
+};
+
+var sendNodeClusterkMessage = function(msg) {
+
+    sendMessage(JSON.stringify(msg));
+};
+
+var sendNodeMessage = function(msg) {
+
+    sendMessage(JSON.stringify(msg));
+};
+
+var sendActionMessage = function(msg) {
+
+    sendMessage(JSON.stringify(msg));
+};
+
+var sendErrorMessage = function(msg) {
+
+    sendMessage(JSON.stringify(msg));
+};
+
 var sendMessage = function(message){
     hipchat.api.rooms.message({
         room_id: HIPCHAT_ROOM,
@@ -38,7 +73,31 @@ ws.on('message', function(event) {
     var msg = JSON.parse(event.data);
     console.log(msg);
 
-    sendMessage(JSON.stringify(msg));
+    if (msg.type == 'container') {
+        sendContainerMessage(msg);
+    }
+    else if (msg.type == 'service') {
+        sendServiceMessage(msg);
+    }
+    else if (msg.type == 'stack') {
+        sendStackMessage(msg);
+    }
+    else if (msg.type == 'nodecluster') {
+        sendNodeClusterkMessage(msg);
+    }
+    else if (msg.type == 'node') {
+        sendNodeMessage(msg);
+    }
+    else if (msg.type == 'action') {
+        sendActionMessage(msg);
+    }
+    else if (msg.type == 'error') {
+        sendErrorMessage(msg);
+    }
+    else {
+        sendMessage(JSON.stringify(msg));
+    }
+
 });
 
 ws.on('close', function(event) {
